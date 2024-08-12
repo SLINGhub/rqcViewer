df3e9911c70f2511b7d36032167fae696088e215
lsijish <lsijish@nus.edu.sg>
2024-08-12 15:49:57 +0800

    Start with trial

--- app.R
+++ app.R
@@ -1,51 +1,82 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(rhandsontable)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+  titlePanel("Data Upload and Filtering with Download Option"),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("data_type", "Choose Data Type:",
+                   choices = c("CSV" = "csv", "Excel" = "excel")),
+      fileInput("file1", "Upload Data File", accept = c(".csv", ".xlsx")),
+      numericInput("n_rows", "Number of rows per page:", 5, min = 1),
+      numericInput("n_cols", "Number of columns per page:", 2, min = 1),
+      downloadButton("download_pdf", "Download PDF"),
+      downloadButton("download_excel", "Download Excel")
+    ),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    mainPanel(
+      rHandsontableOutput("table"),
+      tableOutput("filtered_table")
     )
+  )
 )
 
+
 # Define server logic required to draw a histogram
+library(shiny)
+library(rhandsontable)
+library(readxl)
+library(writexl)
+library(ggplot2)
+library(gridExtra)
+
 server <- function(input, output) {
+  data <- reactive({
+    req(input$file1)
+    if (input$data_type == "csv") {
+      read.csv(input$file1$datapath)
+    } else {
+      read_excel(input$file1$datapath)
+    }
+  })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  output$table <- renderRHandsontable({
+    rhandsontable(data())
+  })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  filtered_data <- reactive({
+    hot_to_r(input$table)
+  })
+
+  output$filtered_table <- renderTable({
+    filtered_data()
+  })
+
+  output$download_pdf <- downloadHandler(
+    filename = function() { "filtered_data.pdf" },
+    content = function(file) {
+      pdf(file)
+      plots <- list()
+      df <- filtered_data()
+
+      for (i in 1:nrow(df)) {
+        p <- ggplot(df[i, , drop = FALSE], aes_string(x = names(df)[1], y = names(df)[2])) +
+          geom_point()
+        plots[[i]] <- p
+      }
+
+      grid.arrange(grobs = plots, nrow = input$n_rows, ncol = input$n_cols)
+      dev.off()
+    }
+  )
+
+  output$download_excel <- downloadHandler(
+    filename = function() { "filtered_data.xlsx" },
+    content = function(file) {
+      write_xlsx(filtered_data(), file)
+    }
+  )
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
